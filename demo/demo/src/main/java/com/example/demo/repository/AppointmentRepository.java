3ea3b405f04ee683dc2dd8d47405305f6749ec7b
shrey-r-j <shreyrj16@gmail.com>
2026-02-12 16:35:57 +0530

    Added basic service layer for appointment, patient

--- demo/demo/src/main/java/com/example/demo/repository/AppointmentRepository.java
+++ demo/demo/src/main/java/com/example/demo/repository/AppointmentRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
     List<Appointment> findByPatientId(Long patientId);
     List<Appointment> findByDoctorId(Long doctorId);
--- demo/demo/src/main/java/com/example/demo/repository/PatientRepository.java
+++ demo/demo/src/main/java/com/example/demo/repository/PatientRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient,Long> {
-    Optional<Patient> findByUserid(Long userId);
 }
